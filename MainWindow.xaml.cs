3c1bd1be67e2ad9840e1a0abd77bb6745ae0233c
Владимир Архипов <Skader-kot@mail.ru>
2023-06-25 16:30:31 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using Newtonsoft.Json;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Documents;
+using System.Resources;
+using System.Reflection;
+using System.Text;
+
+namespace BlockStreamSatAPI
+{
+    public partial class MainWindow : Window
+    {
+        private List<FunctionModel> _functions;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _functions = JsonConvert.DeserializeObject<List<FunctionModel>>(getFuncAndDescJsonFromResources());
+
+            setFunctions(_functions);
+
+        }
+
+        private string getFuncAndDescJsonFromResources()
+        {
+            string textFuncAndDesc = Encoding.UTF8.GetString(StreamBlockSat_InterAPI.Properties.Resources.funcAndDesc);
+            return textFuncAndDesc;
+        }
+
+        private void setFunctions(List<FunctionModel> functions)
+        {
+            foreach (var func in _functions)
+            {
+                var buttonText = new TextBlock() { Text = func.Name, TextWrapping = TextWrapping.Wrap };
+                Button button = new Button { Content = buttonText, Margin = new Thickness(5) };
+
+                button.Click += (sender, e) => { MakeViewForFunction(func); };
+                funcsName.Children.Add(button);
+            }
+        }
+
+        private void MakeViewForFunction(FunctionModel func)
+        {
+            currentFuncName.Text = $"Функция: {func.Name}";
+            currentFuncDesc.Text = func.Desc;
+
+            clearView();
+
+            setParamsToView(func);
+
+
+            Button sendButton = new Button { Content = "Отправить запрос", Margin = new Thickness(5) };
+            sendButton.Click += (sender, e) => {
+                if (isAllRequiredFunctionsFilled(func))
+                {
+                    ExecuteFunction(func);
+                }
+            };
+            sendButtonPlace.Children.Add(sendButton);
+
+        }
+
+        private bool isAllRequiredFunctionsFilled(FunctionModel func)
+        {
+            foreach (var child in funcParamsBox.Children)
+            {
+                if (child is TextBox)
+                {
+                    var paramTextBox = (TextBox)child;
+                    paramTextBox.Background = null;
+                    var param = func.Params.Find(x => x.Name == paramTextBox.Name && x.isRequired == true);
+                    if (param != null)
+                    {
+                        if (paramTextBox.Text.Trim() == "") //в будущем можно добавить валидацию данных в полях
+                        {
+                            paramTextBox.Background = Brushes.Red;
+                            resultTextBlock.Text = $"Ошибка. Не введён обязательный параметр {param.Name}";
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void setParamsToView(FunctionModel func)
+        {
+            if (func.Params.Count > 0)
+            {
+                for (int i = 0; i < func.Params.Count; i++)
+                {
+                    TextBox valueBox = new TextBox { Width = 300, Name = $"{func.Params[i].Name}", Margin = new Thickness(10, 0, 0, 0) };
+                    TextBlock paramName = new TextBlock() { Text = func.Params[i].Name };
+                    if (func.Params[i].isRequired == true)
+                    {
+                        paramName.Inlines.Add(new Run("*") { Foreground = Brushes.Red });
+                    }
+                    paramName.Inlines.Add(new Run(":"));
+
+                    funcParamsBox.Children.Add(paramName);
+                    funcParamsBox.Children.Add(valueBox);
+                }
+                isEmptyParameters.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                isEmptyParameters.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void clearView()
+        {
+            funcParamsBox.Children.Clear();
+            funcParamsBox.Children.Clear();
+            sendButtonPlace.Children.Clear();
+            resultTextBlock.Text = "";
+        }
+
+        private string getResourcesPath()
+        {
+            string appDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+            string parentDirectory = Directory.GetParent(appDirectory).Parent.Parent.FullName;
+            string resourcesDirectory = Path.Combine(parentDirectory, "Resources");
+            return resourcesDirectory;
+        }
+
+        private void ExecuteFunction(FunctionModel func)
+        {
+            if (func.Method == null)
+            {
+                FunctionModel.extractMethodFromUrl(func);
+            }
+
+
+            setParamValuesFromInputBox(func);
+
+            resultTextBlock.Text = prettifyResponse(RestManager.request(func));
+        }
+
+        private string prettifyResponse(string v)
+        {
+            Newtonsoft.Json.Linq.JToken parsedJson = Newtonsoft.Json.Linq.JToken.Parse(v);
+            return parsedJson.ToString(Newtonsoft.Json.Formatting.Indented);
+        }
+
+        private void setParamValuesFromInputBox(FunctionModel func)
+        {
+            foreach (var param in funcParamsBox.Children)
+            {
+                if (param is TextBox)
+                {
+                    var paramTextBox = (TextBox)param;
+                    func.SetParamValue(paramTextBox.Name, paramTextBox.Text.Trim());
+                }
+            }
+        }
+    }
+}
---
+++ RestManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RestSharp;
+
+namespace BlockStreamSatAPI
+{
+    public class Paramameter
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public bool isRequired { get; set; } = false;
+
+        [Newtonsoft.Json.JsonIgnore]
+        public string Value { get; set; } = string.Empty;
+    }
+
+    public class FunctionModel
+    {
+        public string Name { get; set; }
+        public string Desc { get; set; }
+
+        public string URL { get; set; }
+
+        [Newtonsoft.Json.JsonIgnore]
+        public string Method { get; set; } = null;
+        public List<Paramameter> Params { get; set; }
+
+
+        public static void extractMethodFromUrl(FunctionModel func)
+        {
+            func.URL = func.URL.Trim().Replace(" ", "");
+            string method = func.URL.Split('/').ToList()[0];
+            func.Method = method;
+            func.URL = func.URL.Replace(method, "");
+        }
+
+        internal void SetParamValue(string name, string v)
+        {
+            Params.Find(x => x.Name == name).Value = v;
+        }
+    }
+
+    internal class RestManager
+    {
+        public static string request(FunctionModel func)
+        {
+            var method = func.Method;
+            var url = func.URL;
+            var parameters = func.Params;
+
+            RestClient client = new RestClient("https://api.blockstream.space/testnet");
+            RestRequest request;
+
+            switch (method)
+            {
+                case "GET":
+                    request = new RestRequest(url, Method.Get);
+                    break;
+                case "POST":
+                    request = new RestRequest(url, Method.Post);
+                    break;
+                case "PUT":
+                    request = new RestRequest(url, Method.Put);
+                    break;
+                case "DELETE":
+                    request = new RestRequest(url, Method.Delete);
+                    break;
+                default:
+                    request = new RestRequest(url, Method.Head);
+                    break;
+            }
+
+            if (parameters != null)
+            {
+                foreach (var param in parameters)
+                {
+                    if (isValidParameter(param))
+                    {
+                        request.AddParameter(param.Name, param.Value);
+                    }
+                }
+            }
+
+            RestResponse response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                string content = response.Content;
+                return content;
+            }
+            else
+            {
+                string errorMessage = response.ErrorMessage;
+                return errorMessage;
+            }
+        }
+
+        private static bool isValidParameter(Paramameter param)
+        {
+            if (param.Value.Trim() == "")
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
